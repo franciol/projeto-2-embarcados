2f84bc255af81f91871e3b2b85b26758bd7bf14a
franciol <fran.aveiro@hotmail.com>
2019-06-09 18:37:36 -0300

    Add servidor

--- WIFI-RTOS-get-example/python-server/server.py
+++ WIFI-RTOS-get-example/python-server/server.py
@@ -1,27 +1,47 @@
-from flask import Flask, request
+from flask import Flask, request,send_from_directory,send_file
 from flask_restful import Resource, Api
+import flask
+
 
 app = Flask(__name__)
+
+
 api = Api(app)
+
 
 todos = {}
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-#class TodoSimple(Resource):
-#    def get(self, todo_id):
-#        return {todo_id: todos[todo_id]}
-#
-#    def put(self, todo_id):
-#        todos[todo_id] = request.form['data']
-#        return {todo_id: todos[todo_id]}
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-#api.add_resource(TodoSimple, '/<string:todo_id>')
-api.add_resource(HelloWorld, '/')
+@app.route("/512",methods=["GET"])
+def send512():
+    return send_file("a512")
+
+@app.route("/1024",methods=["GET"])
+def send1024():
+    return send_file("a1024")
+
+@app.route("/4096",methods=["GET"])
+def send4096():
+    return send_file("a4096")
+
+@app.route("/8192",methods=["GET"])
+def send8192():
+    return send_file("a8192")
+
+@app.route("/send",methods=["POST"])
+def rec():
+    img_file = request.files.get('file')
+
+    print(img_file.filename)
+    return "ok "+img_file.filename
+
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
-    #app.run(debug=True)
+    app.run(host="0.0.0.0")
+    
 
